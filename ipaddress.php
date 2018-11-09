ade4bf73012b2e307cb063767e7450d1547fddea
cx <chixuchixu@hotmail.com>
2018-11-09 16:37:47 +0800

    save ipaddress if device is not configed

--- ipaddress.php
+++ ipaddress.php
@@ -16,14 +16,19 @@ if($result->num_rows == 0){
   $conn->query(sprintf("update device set login_time='%s' where id = %s", $date, $device_id));
   $location_id = $result->fetch_assoc()["location_id"];
   if(empty($location_id)){
-    $conn->close();
-    die();
-  }
-  if(empty($data["ip"])){
-    $result = $conn->query(sprintf("select ip from location where id = %s", $location_id));
-    echo($result->fetch_assoc()["ip"]);
+    if(empty($data["ip"])){
+      $result = $conn->query("select last_ip from settings");
+      echo($result->fetch_assoc()["last_ip"]);
+    }else{
+      $conn->query(sprintf("update settings set last_ip='%s'", $data["ip"]));
+    }
   }else{
-    $conn->query(sprintf("update location set ip='%s' where id = %s", $data["ip"], $location_id));
+    if(empty($data["ip"])){
+      $result = $conn->query(sprintf("select ip from location where id = %s", $location_id));
+      echo($result->fetch_assoc()["ip"]);
+    }else{
+      $conn->query(sprintf("update location set ip='%s' where id = %s", $data["ip"], $location_id));
+    }
   }
 }
 
